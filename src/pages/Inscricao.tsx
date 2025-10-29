754c5777cb6051f967e3fc07838c9b47dacaddcf
Gabriel Alves de Oliveira <seu-email-do-github@gmail.com>
2025-10-28 23:11:14 -0300

    Alteração 5

--- src/pages/Inscricao.tsx
+++ src/pages/Inscricao.tsx
@@ -12,15 +12,15 @@ const Inscricao = () => {
         </div>
 
         <Card className="p-4 md:p-8">
-          <div className="aspect-video w-full min-h-[1300px]">
+          <div className="aspect-video w-full min-h-[1400px]">
             <iframe
               src="https://docs.google.com/forms/d/e/1FAIpQLSeTEhuMZF688AgllkEo_cfvxL1Dvi9vWofSmeyVyJan-YCaMw/viewform?embedded=true"
-              width="110%"
-              height="1400"
+              width="100%"
+              height="1300"
               frameBorder="0"
               marginHeight={0}
               marginWidth={0}
-              className="w-full h-[1300px] rounded-lg"
+              className="w-full h-[1320px] rounded-lg"
             >
               Carregando…
             </iframe>
